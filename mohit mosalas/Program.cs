43d6920a0e81eb07d8c56abe7ab65ec39d59877c
hamed <blueNoteBook@DESKTOP-2OV0I4P>
2022-11-09 13:38:19 +0330

    Add project files.

---
+++ mohit mosalas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+class mosalas
+{
+    public static void Main(string[] args)
+    {
+        string a= Console.ReadLine();
+        string b= Console.ReadLine();
+        string c= Console.ReadLine();
+        int x= Convert.ToInt32(a);
+        int y= Convert.ToInt32(b);
+        int z= Convert.ToInt32(c);
+        if(x+y>z && x+z>y && y+z>z)
+        {
+            mosalas.Mohit(x,y,z);
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+
+    }
+    private static void Mohit(int x, int y, int z)
+    {
+        int Out = x + y + z;
+        Console.WriteLine(Out);
+    }
+}
